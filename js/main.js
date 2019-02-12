f549d416e45e6a625a22c2215ee8f3469d8846d9
Vitaly Davidenko <35844879+d3zmnd@users.noreply.github.com>
2019-02-13 01:35:38 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function initMap() {
     navigator.geolocation.getCurrentPosition(function(position) {
         var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 2,
+          zoom: 10,
           center: latlng
         });
         marker = new google.maps.Marker({
@@ -13,40 +13,79 @@ function initMap() {
             });
        
     
-        var locations = [];
-        
-        var names = [];
+       function initialize(countries) {
+            var clasterData = [];
+            var data = [];
+             countries.forEach(function(countrie) {
+                 if(countrie.latlng[0] && countrie.latlng[1]) {
+                  data={
+                      position: {
+                          lat: countrie.latlng[0],
+                          lng: countrie.latlng[1],
+                      },
+                      name:countrie.name,
+                      capital: countrie.capital,
+                      population: countrie.population,
+                      alpha3Code: countrie.alpha3Code,
+                      area: countrie.area,
+                      timezones: countrie.timezones,
+                  };
+                 clasterData.push(data);
+              }
+             })
+             return clasterData;
+        }
+
         fetch("https://restcountries.eu/rest/v2/all")
           .then(res => res.json())
           .then(data => initialize(data))
           .then(res => {
-              
-            var markers = res.map(function(location, i) {
+            var markers = res.map(function (marker) {
                 return new google.maps.Marker({
-                  position: location,
-                  
+                  name:marker.name,
+                  position: marker.position,
+                  capital: marker.capital,
+                  population: marker.population,
+                  alpha3Code: marker.alpha3Code,
+                  area: marker.area,
+                  timezones: marker.timezones,
+                  coord: marker.position
                 });
               });
+
+              markers.forEach(function(marker) {
+                var contentString = `
+                <h2>Country: ${marker.name}</h2>
+                <div>Capital: ${marker.capital}</div>
+                <div>Population: ${marker.population}</div>
+                <div>Alpha3Code: ${marker.alpha3Code}</div>
+                <div>Area: ${marker.area} sq. m.</div>
+                <div>Timezones: ${marker.timezones} </div>
+                <div>Coordinates: Lat ${marker.coord.lat}, Lon ${marker.coord.lng}</div>`;
+                
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                  });
+                    marker.addListener('click', function() {
+                      infowindow.open(map, marker);
+                  });
+              })
+
               var markerCluster = new MarkerClusterer(map, markers,
                 {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
           })
           .catch(err => console.log("Error:", err));
-        function initialize(countries) {
-          // console.log(countries)
-          let clasterData = [];
-          let data = [];
-           countries.forEach((countrie) => {
-               if(countrie.latlng[0] && countrie.latlng[1]){
-                console.log(countrie.latlng);
-                data={
-                    lat: countrie.latlng[0],
-                    lng: countrie.latlng[1]
-                   };
-                   clasterData.push(data);
-                }
-            })
-           
-           return clasterData;
-        }
     });
 }
+      
+
+
+
+
+
+
+
+
+
+
+    
