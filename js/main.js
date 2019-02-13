59355625ed9b9bb0375b0526c8e6b541724440ef
Vitaly Davidenko <35844879+d3zmnd@users.noreply.github.com>
2019-02-13 23:38:09 +0200

    Add files via upload
    + autocomplete

--- js/main.js
+++ js/main.js
@@ -3,17 +3,26 @@ function initMap() {
     navigator.geolocation.getCurrentPosition(function(position) {
         var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
+          zoom: 4,
           center: latlng
         });
-        marker = new google.maps.Marker({
+        var marker1 = new google.maps.Marker({
             position: latlng,
             map: map,
             title: "Your current location!"
             });
-       
-    
+
+        var infoWindow = new google.maps.InfoWindow({
+            content:'<h2>Your current location!</h2>'
+        });
+        marker1.addListener('click', function(){
+           infoWindow.open(map, marker1); 
+        })
+        
+        
+           
        function initialize(countries) {
+           
             var clasterData = [];
             var data = [];
              countries.forEach(function(countrie) {
@@ -30,12 +39,13 @@ function initMap() {
                       area: countrie.area,
                       timezones: countrie.timezones,
                   };
-                 clasterData.push(data);
+                  
+                 clasterData.push(data);                          
               }
              })
              return clasterData;
-        }
 
+        }
         fetch("https://restcountries.eu/rest/v2/all")
           .then(res => res.json())
           .then(data => initialize(data))
@@ -51,6 +61,7 @@ function initMap() {
                   timezones: marker.timezones,
                   coord: marker.position
                 });
+
               });
 
               markers.forEach(function(marker) {
@@ -75,6 +86,72 @@ function initMap() {
                 {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
           })
           .catch(err => console.log("Error:", err));
+            var card = document.getElementById('pac-card');
+            var input = document.getElementById('pac-input');
+            map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+            var autocomplete = new google.maps.places.Autocomplete(input);
+            
+            
+            autocomplete.bindTo('bounds', map);
+            autocomplete.setFields(
+                ['address_components', 'geometry', 'name']);
+
+            var infowindow = new google.maps.InfoWindow();
+            var infowindowContent = document.getElementById('infowindow-content');
+            infowindow.setContent(infowindowContent);
+            var marker = new google.maps.Marker({
+              map: map,
+              anchorPoint: new google.maps.Point(0, -29)
+            });
+
+            autocomplete.addListener('place_changed', function() {
+              infowindow.close();
+              marker.setVisible(false);
+              var place = autocomplete.getPlace();
+              if (!place.geometry) {
+                window.alert("No details available for input: '" + place.name + "'");
+                return;
+              }
+              if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+
+              } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(8);  
+
+              }
+              marker.setPosition(place.geometry.location);
+              marker.setVisible(true);
+
+              var address = '';
+              if (place.address_components) {
+                address = [
+                  (place.address_components[0] && place.address_components[0].short_name || ''),
+                  (place.address_components[1] && place.address_components[1].short_name || ''),
+                  (place.address_components[2] && place.address_components[2].short_name || '')
+                ].join(' ');
+              }
+
+              infowindowContent.children['place-name'].textContent = place.name;
+              infowindowContent.children['place-address'].textContent = address;
+              infowindow.open(map, marker);
+
+             // var rad = document.getElementById('radius').value;
+                          
+        //      var circle = new google.maps.Circle({
+        //      strokeColor: '#FF0000',
+        //      map: map,
+        //      radius: rad
+        //      });
+
+        // circle.bindTo('center', marker, 'position');
+    
+            marker.addListener('click', function(){
+
+                infowindow.open(map, marker);
+            })
+
+        });
     });
 }
       
